d802ceca525064b1315d2a38678d5e3ea4f5be14
Mohammad <mdsalam1994@hotmail.com>
2023-03-29 16:01:33 -0400

    student dao

--- app/src/main/java/com/example/g1_mohammad_darosh_lab4/Dao/studentDao.kt
+++ app/src/main/java/com/example/g1_mohammad_darosh_lab4/Dao/studentDao.kt
@@ -1,12 +1,26 @@
 package com.example.g1_mohammad_darosh_lab4.Dao
+import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.example.g1_mohammad_darosh_lab4.entities.Student
 
 
+@Dao
 interface studentDao {
 
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(student : Student)
 
+    @Update
+    suspend fun update(student:Student)
 
+    @Delete
+    suspend fun delete(student: Student)
+
+    @Query("SELECT * FROM studentTable ORDER BY firstname ASC")
+    fun getAllStudents() : LiveData<List<Student>>
+
+// displaying students + their books later
 
 
 
--- app/src/main/java/com/example/g1_mohammad_darosh_lab4/entities/Student.kt
+++ app/src/main/java/com/example/g1_mohammad_darosh_lab4/entities/Student.kt
@@ -1,6 +1,21 @@
 package com.example.g1_mohammad_darosh_lab4.entities
 
-class Student {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "studentTable")
+class Student(@PrimaryKey(autoGenerate = true)@ColumnInfo(name = "firstname") val studentName:String,
+@ColumnInfo(name = "lastname") val lastname:String,
+@ColumnInfo(name = "password") val password :String){
+
+
+
+
+
+
+
+
 
 
     //studentid
